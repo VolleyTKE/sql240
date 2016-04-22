378d86b7e20c92c6a38fef4ef9b35a9d6a730e39
Chris C <chris.a.carr@gmail.com>
2016-04-21 22:07:13 -0500

    ch6 questions through #6

--- lab_3.sql
+++ lab_3.sql
@@ -45,3 +45,64 @@ from invoice_line_items
 group by rollup(account_number);
 
 --lab 3 q #7
+select vendor_name,
+count(distinct li.account_number) as "Number of Accounts"
+from vendors v join invoices i
+on v.vendor_id=i.vendor_id
+join invoice_line_items li
+on i.invoice_id=li.invoice_id
+group by vendor_name
+having count(distinct li.account_number)>1
+order by vendor_name;
+
+--lab 3 ch6 q #1
+select vendor_name
+from vendors
+where vendor_id in
+    (select vendor_id from invoices)
+order by vendor_name;
+
+--lab 3 ch6 q #2
+SELECT invoice_number, invoice_total
+FROM invoices
+WHERE PAYMENT_TOTAL > (select avg(PAYMENT_TOTAL)
+                      from invoices
+                      where payment_total > 0);
+                      
+--lab 3 ch6 q#3
+SELECT account_number, account_description
+FROM GENERAL_LEDGER_ACCOUNTS
+WHERE NOT EXISTS(select * from invoice_line_items
+                  where invoice_line_items.account_number=
+                  general_ledger_accounts.account_number)
+order by account_number;
+
+--lab 4 ch6 q#4
+SELECT vendor_name, i.invoice_id,invoice_sequence,
+      line_item_amt
+FROM vendors v join invoices i
+on v.vendor_id=i.vendor_id
+join invoice_line_items li
+on i.invoice_id=li.invoice_id
+WHERE i.invoice_id in
+        (select invoice_id from invoice_line_items
+        where invoice_sequence>1)
+order by vendor_name, i.invoice_id, invoice_sequence;
+
+--lab 4 ch6 q#5
+select sum(invoice_max) as sum_of_maximums
+from (select vendor_id, max(invoice_total) as invoice_max
+      from invoices
+      where invoice_total-credit_total-payment_total>0
+      group by vendor_id);
+      
+--lab 4 ch6 q#6
+with invoice_max as
+    (select vendor_id, max(invoice_total) as invoice_max
+    from invoices
+    where invoice_total-credit_total-payment_total>0
+    group by vendor_id)
+select sum(invoice_max) as sum_of_maximums
+from invoice_max;
+
+--lab 4 ch6 q#7
